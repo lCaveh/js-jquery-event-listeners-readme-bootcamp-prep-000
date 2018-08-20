f3eb2ebfa4b2438a57302ff70ded3ee1bc5effd6
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-20 19:38:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,7 +8,7 @@ function frameIt() {
   $('img').on('load',
   function(){
     $('img').addClass('tasty');
-    $('img.tasty').css({"border-style": "solid", "border-width": "1 em"});
+    $('img.tasty').css({"border-style": "solid", "border-width": "1 em","border-color": "red"});
   });
 }
 function pressIt() {
