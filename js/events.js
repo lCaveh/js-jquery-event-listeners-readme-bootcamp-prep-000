cc1f975325e4ee757cf08810f35746f43c7dc116
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-20 19:28:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,24 @@
-//define functions here
+function getIt() {
+  $('p').on('click',
+  function(){
+    alert("Hey!");
+  });
+}
+function frameIt() {
+  $('img').on('load',
+  function(){
+    $('img').addClass('tasty');
+    $('img.tasty').css({"border-style": "solid", "border-width": "1 em"});
+  });
+}
+function pressIt() {
+  $('input').on('keydown',
+  function(key){
+    if (key.which == 71) {
+    alert("G key was pressed down!");
+    }
+  });
+}
 
 $(document).ready(function(){
 
